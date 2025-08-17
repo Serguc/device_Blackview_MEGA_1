9df8dfd239a61df378e65e5adc6baaf4f2a1291e
serguc <42298249+Serguc@users.noreply.github.com>
2025-08-17 06:53:23 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -145,6 +145,9 @@ TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
 PRODUCT_COPY_FILES += \
      device/Blackview/MEGA_1/fstab.mt6789:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.mt6789
 
+PRODUCT_COPY_FILES += \
+     device/Blackview/MEGA_1/fstab.emmc:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.emmc
+
 # OEM otacerts
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     $(DEVICE_PATH)/security/otacert

ee9cac8be14a38376c187ddd34009175ca0822cd
serguc <42298249+Serguc@users.noreply.github.com>
2026-01-04 14:29:02 +0200

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,6 +16,8 @@
 
 PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_MEGA_1.mk
 
-
-COMMON_LUNCH_CHOICES := twrp_MEGA_1-eng
-
+COMMON_LUNCH_CHOICES := \
+    twrp_MEGA_1-user \
+    twrp_MEGA_1-userdebug \
+    twrp_MEGA_1-eng
+#

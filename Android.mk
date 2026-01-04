14b098b8aac2681f94d7f51d07cb0838d532d89a
serguc <42298249+Serguc@users.noreply.github.com>
2026-01-04 14:26:26 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -5,6 +5,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-soong_namespace {
-    imports: ["hardware/mtk-caf/bootctrl"],
-}
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),MEGA_1)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
